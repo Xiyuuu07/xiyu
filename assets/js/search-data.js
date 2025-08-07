5c825e21db6620d855ee2ca637eed419b39b529b
Xiyuuu07 <106743847+Xiyuuu07@users.noreply.github.com>
2025-08-07 08:24:56 +0000

    Deploying to gh-pages from @ Xiyuuu07/xiyu@eaf2b321a0d1028f63873644b8db343886779b4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/xiyu/cv/";
